b49e37b4453a37bb94e5cdbbb745bf8a7097da4f
Linsong Wang <tascape@users.noreply.github.com>
2016-02-07 16:07:19 -0800

    Update run-smoke-suite.sh

--- run-smoke-suite.sh
+++ run-smoke-suite.sh
@@ -3,4 +3,5 @@
 echo 'run "mvn clean package" if needed'
 
 java -cp target/*:target/th/*:target/dependency/* \
+  -Dqa.th.comm.ADB_EXECUTABLE=/usr/local/bin/adb
 com.tascape.qa.android.demo.suite.SmokeSuite
